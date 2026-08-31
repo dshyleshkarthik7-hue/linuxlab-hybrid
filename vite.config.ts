9f0915100f057f60411e41cda0abf568f60127d4
Dinavahi Shylesh Karthik <dshyleshkarthik7@gmail.com>
2026-08-31 17:33:30 +0530

    build: use esbuild CSS minifier for compatibility

--- vite.config.ts
+++ vite.config.ts
@@ -27,6 +27,7 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     target: 'esnext',
+    cssMinify: 'esbuild',
     chunkSizeWarningLimit: 1000,
     rollupOptions: {
       input: {
